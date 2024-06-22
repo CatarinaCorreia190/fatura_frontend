851821c37bdc3cca606b89ff42f526f7d69a83ee
fanuel.ramos <fanuel.ramos@alien-group.com>
2024-06-22 23:43:47 +0100

    big changes

---
+++ ImprimeFatura/js.js
@@ -0,0 +1,13 @@
+document.getElementById('downloadPDF').addEventListener('click', function () {
+    const { jsPDF } = window.jspdf;
+    html2canvas(document.getElementById('invoice')).then(function (canvas) {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        const imgProps = pdf.getImageProperties(imgData);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        pdf.save('invoice.pdf');
+    });
+});
